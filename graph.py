4f6bf640c802b8bdd1f3f81f72070d8a403b7a17
sion gang <siongang@gmail.com>
2023-08-04 00:12:40 -0400

    intial

---
+++ graph.py
@@ -0,0 +1,69 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import os
+
+G = nx.Graph()
+
+map = {}
+
+image_path = "graph.png"
+
+def draw():
+    # Draw the graph
+    pos = nx.spring_layout(G)  # Layout algorithm for graph visualization
+    nx.draw(G, pos, with_labels=True, node_size=1000, node_color="skyblue", font_size=10, font_weight="bold", edge_color="gray", width=2, edge_cmap=plt.cm.Blues)
+        
+    # Add edge weights to the graph visualization
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_color='red')
+  
+    if os.path.isfile(image_path):
+       
+       os.remove(image_path)
+      
+
+    plt.savefig("graph.png")
+    plt.clf()
+    print(map)
+
+def check(person1, person2):   
+    global map 
+    for p1 in map:
+        if p1 == person1:
+            mainNeighbors = map[p1]
+            for mainSub in mainNeighbors:
+                if mainSub == person2:
+                    return False
+    return True
+
+
+def getWeight(person1, person2):
+    global map
+    if not check(person2, person1):
+        return 2
+    return 1
+
+
+def connect(person1, person2):
+    global map
+    weight = ''
+    if check(person1, person2):
+        weight = getWeight(person1, person2)
+       
+        addToMap(person1,person2)
+        G.add_edge(person1, person2, weight=weight)
+        draw()
+
+
+def addToMap(person1, person2):
+    weight = getWeight(person1, person2)
+    global map
+
+    if person1 in map:
+        neighbors = map[person1]
+        neighbors.append(person2)
+        map[person1] = neighbors
+    else:
+       map[person1] = [person2]
+
+
---
+++ script.py
@@ -0,0 +1,76 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+import graph
+
+load_dotenv()
+token = os.getenv('TOKEN')
+image_path = "graph.png"
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+intents.guilds = True
+intents.guild_messages = True
+intents.presences = True
+
+bot = commands.Bot(command_prefix=["!","stt "], intents=intents)
+
+# Custom HelpCommand
+class CustomHelpCommand(commands.HelpCommand):
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Bot Commands",description="Spill the tea in ur friend group!\n`The bot will map the 'connections' between ppl.\nif the value between 2 ppl is 1, it is 1 sided,\notherwise, if it is 2, its mu2al!`\n\n`bot prefix: '!' or 'stt '`", color=discord.Color.blue())
+
+
+        instruction = "`type stt like [mention]`"
+        embed.add_field(name="How to use !like", value=instruction, inline=False)
+
+        commands = [  
+            {"name": "like", "brief": "Submit your tea (smone u like) :)"},
+            {"name": "reset", "brief": "Resets the image."},
+            {"name": "help", "brief": "Shows the list of available commands."}
+        ]
+
+        command_list = "\n".join([f"`{cmd['name']}`: {cmd['brief']}" for cmd in commands])
+        embed.add_field(name="Commands", value=command_list, inline=False)
+
+        await self.get_destination().send(embed=embed)
+
+
+
+
+# Set the custom help command
+bot.help_command = CustomHelpCommand()
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot is ready. Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author != bot.user:
+        await bot.process_commands(message)
+
+
+@bot.command(brief="submit your crush")      
+async def like(ctx, member: discord.Member):
+    person1 = ctx.author.name
+    person2 = member.name
+    graph.connect(person1,person2)
+    image = discord.File(image_path)
+    await ctx.send(file=image)
+
+@bot.command(brief="reset the image")
+async def reset(ctx):
+    if os.path.exists(image_path):
+        os.remove(image_path)
+        await ctx.send("map has been reset")
+    else:
+        await ctx.send(f"{image_path} does not exist.")
+
+
+
+bot.run('MTA0MjA5MjM1NzIxNjEwODU2NA.GwmyVZ.91O2vaT8l9UmaneXBB3qbC6za6I-CyiRwts9QE')
